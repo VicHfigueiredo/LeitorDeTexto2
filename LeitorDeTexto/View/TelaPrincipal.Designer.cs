2cc09e2bc455fd339f585ab7b6e90b25082aeec7
victor.hfigueiredo <victor.hfigueiredo@SBC0505249W10-1>
2022-05-13 16:34:56 -0300

    Adicionar arquivos de projeto.

---
+++ LeitorDeTexto/View/TelaPrincipal.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace LeitorDeTexto.View
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            this.btnLer = new System.Windows.Forms.Button();
+            this.ltbLeitor = new System.Windows.Forms.ListBox();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLer
+            // 
+            this.btnLer.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLer.Location = new System.Drawing.Point(152, 74);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(509, 28);
+            this.btnLer.TabIndex = 0;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // ltbLeitor
+            // 
+            this.ltbLeitor.FormattingEnabled = true;
+            this.ltbLeitor.ItemHeight = 15;
+            this.ltbLeitor.Location = new System.Drawing.Point(41, 137);
+            this.ltbLeitor.Name = "ltbLeitor";
+            this.ltbLeitor.Size = new System.Drawing.Size(712, 274);
+            this.ltbLeitor.TabIndex = 1;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(195, 18);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(437, 28);
+            this.lblTitulo.TabIndex = 2;
+            this.lblTitulo.Text = "Leitor de texto com informações delimitadas";
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(808, 468);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.ltbLeitor);
+            this.Controls.Add(this.btnLer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TelaPrincipal";
+            this.Text = " Leitor de Texto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.ListBox ltbLeitor;
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
---
+++ LeitorDeTexto/View/TelaPrincipal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeitorDeTexto.View
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            string endereco = @"C:\senac\teste007";
+            try
+            {
+                if (!System.IO.File.Exists(endereco))
+                {
+                    new Exception(
+                    "Arquivo teste007.txt não foi localizado.");
+                }
+                string[] dados = new string[4];
+                System.IO.StreamReader Leitor = new System.IO.StreamReader(endereco);
+
+                ltbLeitor.Items.Add(
+                    "Código".PadRight(7) +
+                    "Cliente".PadRight(40) +
+                    "Cidade".PadRight(20) +
+                    "País"
+                    );
+                ltbLeitor.Items.Add(new string('-', 80));
+
+                while (!Leitor.EndOfStream)
+                {
+                    dados = Leitor.ReadLine().Split(';');
+                    ltbLeitor.Items.Add(
+                        dados[0].PadRight(7) +
+                        dados[1].PadRight(40) +
+                        dados[2].PadRight(20) +
+                        dados[3]);
+                }
+                Leitor.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw; 
+            }
+        }
+    }
+}
